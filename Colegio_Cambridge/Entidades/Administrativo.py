f5aefa5a266a7b86ebc117ed8dbc4fc38c1fca9d
GarroDev <160985704+GarroDev@users.noreply.github.com>
2025-09-17 18:29:09 -0500

    Delete Colegio_Cambridge directory

--- Colegio_Cambridge/Entidades/Administrativo.py
+++
@@ -1,5 +0,0 @@
-from Entidades.Persona import Persona
-
-class Administrativo(Persona):
-    def __init__(self, id, area, oficina):
-        super().__init__(id, "Administrativo", area, oficina)
--- Colegio_Cambridge/Entidades/Profesor.py
+++
@@ -1,9 +0,0 @@
-from Entidades.Persona import Persona
-
-class Profesor(Persona):
-    def __init__(self, id, area, oficina, tipo_profesor):
-        super().__init__(id, "Profesor", area, oficina)
-        self.tipo_profesor = tipo_profesor  # "Planta" o "Contratista"
-    
-    def __str__(self):
-        return f"{super().__str__()}, Tipo Profesor: {self.tipo_profesor}"
